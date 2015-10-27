843b3ad7d39b24f2897f5cb2fb7d6ea4c2bade27
Eduardo <eduzribeiro@gmail.com>
2015-10-27 16:29:20 -0200

    Primeiro Teste FIR/Integral

---
+++ src/Integral.java
@@ -0,0 +1,14 @@
+
+public class Integral {
+	double last_g=0;
+	double last_t=0;
+	double EvaluateValue(double f,double t)
+	{
+		double g;
+		
+		g=f*(t-last_t)+last_g;
+		last_g=g;
+		last_t=t;
+		return g;
+	}
+}
